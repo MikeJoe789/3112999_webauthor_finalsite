80fbd2f95bcd8cf1409bbfc0824ffa7060896ac7
MikeJoe789 <juntaoli2022@gmail.com>
2024-01-02 16:54:50 +0000

    banner

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let menuButton=document.querySelector('#menuButton')
-let menu = document.querySelector('#header nav ul')
+let menu = document.querySelector('#header nav')
 menuButton.addEventListener("click",()=>{
     menu.classList.toggle("open")
 })
+
+new Splide( '#image-carousel').mount();
