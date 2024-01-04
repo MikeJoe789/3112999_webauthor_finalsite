443ae34e7a8dadd445d51898e173cddc51182af0
MikeJoe789 <juntaoli2022@gmail.com>
2024-01-04 11:53:15 +0000

    0104

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-let menuButton=document.querySelector('#menuButton')
-let menu = document.querySelector('#header nav')
-menuButton.addEventListener("click",()=>{
-    menu.classList.toggle("open")
-})
+// menu button begin
+let menuButton = document.querySelector("#menuButton");
+let menu = document.querySelector("#header nav");
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+// menu button end
 
-new Splide( '#image-carousel').mount();
+// splide begin
+new Splide("#image-carousel").mount();
+
+// splide end
+
+// timer begin
+let countDownDate = new Date("July 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function () {
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML =
+    days + "D  " + hours + "H  " + minutes + "M  " + seconds + "S";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+// timer end
